ac6182730487bd6b5737ad55f8f4d203b669e4ba
RyuTa <r.tae.su.0625@gmail.com>
2024-06-21 18:19:06 +0900

    240621/DP/BOJ_11053/가장 긴 증가하는 부분 수열

--- Algorithm/DP_BOJ_11726_2xN타일링.py
+++ Algorithm/DP_BOJ_11726_2xN타일링.py
@@ -1,11 +1,9 @@
 n = int(input())
-# memoization을 위함
-cache = [0]*1001
-# n이 1,2인 경우는 명확하니까 미리 선언해둔다.
-cache[1]=1
-cache[2]=2
-# dynamic programming
-for i in range(3,1001):
-  cache[i] = (cache[i-1]+cache[i-2])%10007
+dp = [0] * 1001
+dp[1] = 1
+dp[2] = 2
 
-print(cache[n])
+for i in range(3, n + 1):
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 10007
+
+print(dp[n])
