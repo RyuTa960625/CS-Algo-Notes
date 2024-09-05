2f5e19854712fc9831e1731432996459330112e2
류태수 <r.tae.su.0625@gmail.com>
2024-09-05 19:24:51 +0900

    240905/DP/BOJ_11660/구간 합 구하기 5/시간초과 재시도

--- Algorithm/DP_BOJ_11660_구간합 구하기 5.py
+++ Algorithm/DP_BOJ_11660_구간합 구하기 5.py
@@ -1,24 +1,15 @@
-N, M = map(int, input().split())
-matrix = []
-for i in range(N):
-    matrix.append(list(map(int, input().split())))
+import sys
+input = sys.stdin.readline
 
-prefix_sum_matrix = [[0 for _ in range(N)] for _ in range(N)]
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+sum = [[0] * (n+1) for _ in range(n+1)]
 
-prefix_sum_matrix[0][0] = matrix[0][0]
-for j in range(1,N):
-    prefix_sum_matrix[0][j] = prefix_sum_matrix[0][j-1] + matrix[0][j]
-for j in range(1,N):
-    prefix_sum_matrix[j][0] = prefix_sum_matrix[j-1][0] + matrix[j][0]
-for i in range(1,N):
-    for j in range(1,N):
-        prefix_sum_matrix[i][j] = prefix_sum_matrix[i][j-1] + prefix_sum_matrix[i-1][j] - prefix_sum_matrix[i-1][j-1] + matrix[i][j]
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        sum[i][j] = arr[i-1][j-1] + sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1]
 
-for i in range(N):
-    prefix_sum_matrix[i] = [0] + prefix_sum_matrix[i]
-zero_padding = [[0 for _ in range(N+1)]]
-prefix_sum_matrix = zero_padding + prefix_sum_matrix
-
-for k in range(M):
+for _ in range(m):
     x1, y1, x2, y2 = map(int, input().split())
-    print(prefix_sum_matrix[x2][y2] - prefix_sum_matrix[x2][y1-1] - prefix_sum_matrix[x1-1][y2] + prefix_sum_matrix[x1-1][y1-1])
+    result = sum[x2][y2] - sum[x2][y1-1] - sum[x1-1][y2] + sum[x1-1][y1-1]
+    print(result)
