0bc5965433af4059019328da7554878b0d53015c
류태수 <r.tae.su.0625@gmail.com>
2024-10-07 23:39:43 +0900

    241007/DP/BOJ_10844/쉬운 계단 수

---
+++ Algorithm/DP/BOJ_10844_쉬운 계단 수.py
@@ -0,0 +1,17 @@
+N = int(input())
+
+arr = [[0] * 10 for _ in range(N + 1)]
+
+for i in range(1, 10):
+    arr[1][i] = 1
+
+for i in range(2, N + 1):
+    for j in range(10):
+        if j == 0:
+            arr[i][j] = arr[i - 1][1]
+        elif j == 9:
+            arr[i][j] = arr[i - 1][8]
+        else:
+            arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j + 1]
+
+print(sum(arr[N]) % 1000000000)
